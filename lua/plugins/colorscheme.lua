c7f3a7c0d1300a9cd202534d9c3647ff1680649e
HyeonTee <gusxo1820@icloud.com>
2024-09-04 18:39:45 +0900

    plugin: Noice

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
 }
