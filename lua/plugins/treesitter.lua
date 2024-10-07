28b4353baa7d355e8f1402ec59bd69889dee71d2
HyeonTee <gusxo1820@icloud.com>
2024-10-07 10:56:10 +0900

    treesitter.lua

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "rust",
+        "sql",
+        "svelte",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
